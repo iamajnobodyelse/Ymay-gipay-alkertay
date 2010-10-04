969219c7af767169423cc8d6dc4b32a0be3942b2
iamajnobodyelse <iamajnobodyelse@gmail.com>
2010-10-04 00:25:54 -0700

    Breaking down the sentence

---
+++ Ymay-gipay-alkertay/Ymay_gipay_alkertay.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ymay_gipay_alkertay
+{
+    class Ymay_gipay_alkertay
+    {
+        static void Main(string[] args)
+        {
+            string input;
+
+            Console.WriteLine("Enter a sentence to translate.");
+            input = (Console.ReadLine()).ToLower();
+            string[] words = input.Split(' ');
+            foreach (string output in words)
+            {
+                Console.Write(output + " ");
+            }
+            Console.ReadKey(true);
+        }
+    }
+}
