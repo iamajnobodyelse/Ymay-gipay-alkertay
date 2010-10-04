64289e53ecd2e343f95efddda4701db37b9db376
iamajnobodyelse <iamajnobodyelse@gmail.com>
2010-10-04 12:26:57 -0700

    HaHA.... Success

--- Ymay-gipay-alkertay/Ymay_gipay_alkertay.cs
+++ Ymay-gipay-alkertay/Ymay_gipay_alkertay.cs
@@ -13,6 +13,7 @@ namespace Ymay_gipay_alkertay
 
             Console.WriteLine("Enter a sentence to translate into Pig Latin.");
             input = (Console.ReadLine()).ToLower();
+            Console.WriteLine("In Pig Latin that's:");
             string[] words = input.Split(' ');
             foreach (string output in words)
             {
@@ -38,46 +39,12 @@ namespace Ymay_gipay_alkertay
                 }
                 else
                 {
-                    int[] array = new int[5];
-                    array[0] = output.IndexOf('a');
-                    array[1] = output.IndexOf('e');
-                    array[2] = output.IndexOf('i');
-                    array[3] = output.IndexOf('o');
-                    array[4] = output.IndexOf('u');
-                    // find the vowel that shows up first
-                    int testOne = Array.IndexOf<int>(array, 1);
-                    int testTwo = Array.IndexOf<int>(array, 2);
-                    int testThree = Array.IndexOf<int>(array, 3);
-                    int testFour = Array.IndexOf<int>(array, 4);
-                    int testFive = Array.IndexOf<int>(array, 5);
-
-                    Console.Write(testOne);
-                    Console.Write(testTwo);
-                    Console.Write(testThree);
-                    Console.Write(testFour);
-                    Console.Write(testFive);
-                    Console.WriteLine("");
-
-                    int vowelCheck = output.IndexOfAny("aeiou".ToCharArray()) != -1;
+                    int vowelCheck = output.IndexOfAny("aeiou".ToCharArray());
                     string partOne = output.Substring(vowelCheck);
                     string partTwo = output.Substring(0,vowelCheck);
                     string translated = String.Concat(partOne, partTwo);
                     Console.Write(translated + "ay ");
-
-                    /*
-                    if ((output.IndexOf('a') = -1) && (output.IndexOf('e') = -1) && (output.IndexOf('i') = -1) && (output.IndexOf('o') = -1) && (output.IndexOf('o') = -1) && (output.IndexOf('u') = -1))
-                    {
-                        Console.WriteLine("\n\aNext time type real words Dummy!");
-                        Console.ReadKey(true);
-                        break;
-                    }
-                    else
-                    {
-                        Console.WriteLine("\n\a\a**Argh! You broke me!**\n(That means something went wrong)");
-                        Console.ReadKey(true);
-                        break;
-                    }
-                    */
+                   
                 }
             }
             Console.ReadKey(true);
